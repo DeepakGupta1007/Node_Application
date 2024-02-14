3ea17caa8fb3a3ec93a0ad4d412c793cf82a742e
Deepak <deepakanjanigupta432@gmail.com>
2024-02-14 22:14:22 +0530

    HTTP Request completed

--- httpmodule.js
+++ httpmodule.js
@@ -2,11 +2,19 @@
 const { count } = require('console');
 const  http =require('http');
 
-const server = http.createServer()
-
-server.on('connection',(socket)=>{
-    console.log("New Connection");
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write('Hello World');
+        res.end();
+    }
+    if(req.url==='/api/username'){
+        res.write('Deepak Gupta');
+        res.end();
+    }
 })
+
+
+//This http module is not used now. We use express which is bbuild on top of http module.
 
 server.listen(3000,()=>{
     console.log("Listening on 3000");
