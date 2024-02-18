f9651c5dfa1e62d926f248a5d0a365a3db05125c
Deepak <deepakanjanigupta432@gmail.com>
2024-02-18 16:14:54 +0530

    Callback Hell

--- JS_Concepts/index.js
+++ JS_Concepts/index.js
@@ -5,21 +5,34 @@ const getUser=(id,callback)=>{
     },2000)
 };
 
-//Example ofAsync Code 
-console.log("Before");
-getUser(1,(user)=>{
-    getRepositories(user.userName,(obj)=>{
-        obj.forEach(data => {
-            console.log(data);
-        });
-    });
-});
-
-console.log('After');
-//Assesment APi to fetch the repos of the username
 function getRepositories(userName,callback){
     setTimeout(()=>{
         console.log('Calling Git Api....');
         callback(['A','B','C']);
     },2000);
 }
+
+
+//Example ofAsync Code 
+console.log("Before");
+getUser(1,(user)=>{
+    getRepositories(user.userName,(obj)=>{
+        obj.forEach(repo => {
+            console.log(repo);
+            //Now if we want to get the commit for each repo
+            getCommits(repo,(commits)=>{
+                //Callback hell is the problem occured if followed the above approach.
+            });
+        });
+    });
+});
+console.log('After');
+
+
+
+//It the above code is synchronous 
+// console.log('Before');
+// const user =getUser(1);
+// const repos=getRepositories(user.userName);
+// const commits=getCommits(repos[0]);
+// console.log('After');
