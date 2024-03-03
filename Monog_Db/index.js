a5dd63fc42d323dc0f4added706d3507086a9d8a
Deepak <deepakanjanigupta432@gmail.com>
2024-03-03 21:08:21 +0530

    Applying queries in Mongo

--- Monog_Db/index.js
+++ Monog_Db/index.js
@@ -25,24 +25,14 @@ const courseSchema = new mongoose.Schema({
 
 
 //Creating a class
- const Course = mongoose.model('Course',courseSchema);
-const course = new Course({
-    name: 'Node.js',
-    author: 'Deepak',
-    tags: ['node','mongodb'],
-    isPublished: true
-});
-
-// Alternate-1
-course.save()
-.then(()=>console.log('Data Saved..'))
-.catch((err)=>console.log("Error"));
+const Course = mongoose.model('Course',courseSchema);
 
 //Alternate-2
+//This function is used to insert the course
 async function createCourse(){
     const course = new Course({
         name: 'React.js',
-        author: 'Deepak Gupta',
+        author: 'Deepak',
         tags: ['Redux','reactJS'],
         isPublished: true
     });
@@ -52,4 +42,27 @@ async function createCourse(){
 
 }
 
-createCourse();
+//This function is used to query the courses -with the given filter
+async function getCourses(){
+    const courses = await Course.find({author: 'Deepak Gupta',isPublished:false});
+    console.log(courses.length);
+}
+// getCourses();
+
+//Function to get the specific data not whole object
+async function getCoursesSpecificData(){
+    const courses = (await Course.find()
+    .limit(3)
+    .sort({name:1})
+    .select({name:1}));
+    console.log(courses);
+}
+getCoursesSpecificData();
+
+async function removeCourses(){
+    const result = await Course.deleteMany();
+    console.log(result);
+    getCourses();
+}
+// removeCourses();
+// createCourse();
